849e497838f6007345f4577b2a8c51dc9b520dce
ajgmez1 <ajgmez1@gmail.com>
2020-05-15 15:27:03 -0700

    add defaultColors, testAll; fix frontend bugs

--- src/main/webapp/src/main.js
+++ src/main/webapp/src/main.js
@@ -4,11 +4,8 @@
     var Helper = (function() {
         return {
             request: function(url, opts, fn) {
-                fetch(new Request(url, opts)).then(function(response) {
-                    response.json().then(fn).catch(function(e) {
-                        console.error(url, response.status, e);
-                    });
-                });
+                return fetch(new Request(url, opts)).then((r) => r.json()).then(fn)
+                        .catch((e) => console.error(url, response.status, e));
             },
             createSelectHTML: function(dom, values) {
                 var option, i;
@@ -54,49 +51,76 @@
         }
     });
 
-    // global events
-    document.getElementById('addForm').addEventListener('click', function(e) {
-        e.preventDefault();
-        Form.createForm();
-    });
-
     var Form = (function() {
-        var inputSize = document.querySelector('[name=inputSize]');
         var container = document.getElementById('container');
-        var ids = {
-            next: 1,
+        var master = document.getElementById('master-form');
+        var addForm = master.querySelector('#addForm');
+        var testAll = master.querySelector('#testAll');
+        var inputSize = master.querySelector('#inputSize');
+        var config = {
+            next: 0,
             max: 5,
-            forms: []
+            forms: [],
+            defaultColors: ['#1779ba', '#3adb76', '#a53b2a', '#cc8b00', '#767676']
         };
 
+        // master form events
+        addForm.addEventListener('click', () => {
+            if (!addForm.hasAttribute('disabled')) {
+                Form.createForm();
+            }
+        });
+        testAll.addEventListener('click', () => testAllFn());
+        inputSize.addEventListener('change', () => testAllFn());
+
+        // private methods
+        const testAllFn = () => {
+            const reqs = [], fields = [];
+            config.forms.forEach((f) => {
+                var submit = f.querySelector('[name=submit]');
+                reqs.push(submit.onclick);
+                fields.push(submit);
+            });
+
+            Promise.all(reqs.map((req) => req({ bulk: true }))).then(() => {
+                chart.update();
+                chart.options.animation.onComplete = () => {
+                    disable(false, fields.concat([testAll, inputSize]));
+                }
+            });
+        };
         const addEvents = function(form) {
-            var id = form.id;
             var collections = form.querySelector('[name=collections]');
             var operations = form.querySelector('[name=operations]');
+            var submit = form.querySelector('[name=submit]');
+            var deleteBtn = form.querySelector("[name=delete]");
             var color = form.querySelector('[name=color]');
+
+            // initialize fields
+            const setColors = config.forms.map((f) => f.color);
+            color.value = config.defaultColors.filter((c) => !setColors.includes(c))[0];
+            form.color = color.value;
 
             // Events
             collections.onchange = () => {
-                if (operations.c) {
-                    const values = operations.c[collections.value].methods.map((m) => m.type);
-                    Helper.createSelectHTML(operations, values);
-                    operations.onchange();
-                }
+                const values = operations.c[collections.value].methods.map((m) => m.type);
+                Helper.createSelectHTML(operations, values);
+                operations.onchange();
             };
-
             operations.onchange = () => {
-                if (operations.c) {
-                    const methods = operations.c[collections.value].methods;
-                    const { type, name, description } = methods.find((m) => m.type === operations.value);
-                    // TODO: show description somewhere!
-                    console.log(type, name, description);
-                }
+                const methods = operations.c[collections.value].methods;
+                const { type, name, description } = methods.find((m) => m.type === operations.value);
+                // TODO: show description somewhere!?
+                console.log(type, name, description);
             };
-            // POST api/collection/test
-            form.querySelector('[name=submit]').addEventListener('click', function(e) {
-                e.preventDefault();
+            color.onchange = () => {
+                form.color = color.value;
+            };
+            submit.onclick = (e) => {
+                if (submit.hasAttribute('disabled')) return;
+                disable(true, [submit, inputSize, testAll]);
 
-                Helper.request('api/collection/test', {
+                return Helper.request('api/collection/test', {
                     method: 'POST',
                     headers: {'Content-Type': 'application/json'},
                     body: JSON.stringify({
@@ -104,80 +128,83 @@
                         operation: operations.value,
                         inputSize: inputSize.value
                     })
-                }, function(json) {
+                }, (json) => {
                     chart.data.labels = [];
-                    chart.data.datasets[id] = {};
-                    chart.data.datasets[id].label = collections.value + ' - ' + operations.value;
-                    chart.data.datasets[id].borderColor = color.value;
-                    chart.data.datasets[id].data = [];
+                    chart.data.datasets[form.id] = {};
+                    chart.data.datasets[form.id].label = collections.value + ' - ' + operations.value;
+                    chart.data.datasets[form.id].borderColor = color.value;
+                    chart.data.datasets[form.id].data = [];
                     for (var n in json) {
                         chart.data.labels.push(n);
-                        chart.data.datasets[id].data.push({ x:n, y:json[n] });
+                        chart.data.datasets[form.id].data.push({ x:n, y:json[n] });
                     }
-                    chart.update();
-                });
-            });
 
-            form.querySelector("[name=delete]").addEventListener('click', function(e) {
-                e.preventDefault();
-                deleteForm(parseInt(id));
+                    if (!e.bulk) {
+                        chart.update();
+                        chart.options.animation.onComplete = () => {
+                            disable(false, [submit, inputSize, testAll]);
+                        }
+                    }
+                });
+            };
+
+            deleteBtn.addEventListener('click', (e) => {
+                deleteForm(parseInt(form.id));
             });
 
             // GET select options
-            Helper.request('api/collections', {}, (r) => {
-                Helper.createSelectHTML(collections, r.Collections);
-                collections.onchange();
-            });
-            Helper.request('api/operations', {}, (r) => {
-                operations.c = r;
+            Promise.all([
+                Helper.request('api/collections', {}, (r) => r),
+                Helper.request('api/operations', {}, (r) => r)
+            ]).then(([c, o]) => {
+                Helper.createSelectHTML(collections, c.Collections);
+                operations.c = o;
                 collections.onchange();
             });
         };
         const deleteForm = function(id) {
-            var forms = container.querySelectorAll('form');
-            var form = ids.forms[id];
+            var form = config.forms[id];
 
-            if (form && forms.length > 1) {
+            if (form && config.forms.length > 0) {
                 form.remove();
-                ids.forms[id] = false;
-                if (id < ids.next) {
-                    ids.next = id;
-                }
+                
+                config.forms.splice(id, 1);
+                config.forms.forEach((f, i) => f.id = i);
+                config.next = config.forms.length;
+
+                disable(false, [addForm]);
+
                 chart.data.datasets.splice(id, 1);
                 chart.update();
             }
-            
-            console.warn(ids);
         };
-
-        // add first form events
-        const form = container.querySelector('form');
-        ids.forms[0] = form;
-        addEvents(form);
+        const disable = (d, els) => {
+            els.forEach((e) => {
+                if (d) {
+                    e.setAttribute('disabled', '');
+                } else {
+                    e.removeAttribute('disabled');
+                }
+            });
+        }
 
         return {
             createForm: function() {
-                var id = ids.next;
-                var form = container.querySelector('form');
-                
-                if (id < ids.max) {
-                    var newForm = form.cloneNode(true);
-                    newForm.id = id;
-                    container.appendChild(newForm);
+                var id = config.next;
+                var form = container.querySelector('#form-template');
+                var newForm = form.cloneNode(true);
 
-                    ids.forms[id] = newForm;
-                    var newId = ids.forms.findIndex((v) => !v);
-                    if (newId === -1) {
-                        ids.next = ids.forms.length;
-                    } else {
-                        ids.next = newId;
-                    }
+                newForm.id = id;
+                container.appendChild(newForm);
 
-                    addEvents(newForm);
-                }
-                
-                console.warn(ids);
+                config.forms[id] = newForm;
+                config.next = config.forms.length;
+
+                disable(config.next === config.max, [addForm]);
+                addEvents(newForm);
             }
         };
     })();
+
+    Form.createForm();
 })();
